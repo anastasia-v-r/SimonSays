8113cad690e3c421bd4ebd4c3e47f1d8a4b476f4
Dominik Rutter <dominik.rutter@gmail.com>
2023-11-02 00:28:05 -0400

    Add basic boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <random>
+
 #include <SFML/Graphics.hpp>
 
 int main()
@@ -7,14 +10,38 @@ int main()
 
     while (window.isOpen())
     {
+        // Process Input
         for (auto event = sf::Event{}; window.pollEvent(event);)
         {
-            if (event.type == sf::Event::Closed)
+            switch (event.type) 
             {
+            case sf::Event::Closed:
                 window.close();
+                break;
+            case sf::Event::KeyPressed:
+                switch (event.key.code)
+                {
+                case sf::Keyboard::Escape:
+                    window.close();
+                    break;
+                case sf::Keyboard::Backspace:
+                    window.setSize(sf::Vector2u{window.getSize().x - 100, window.getSize().y});
+                    break;
+                case sf::Keyboard::Space:
+                    window.setSize(sf::Vector2u{ window.getSize().x + 100, window.getSize().y });
+                    break;
+                default:
+                    window.setTitle(std::to_string([]() {std::random_device rd; std::mt19937 mt(rd()); std::uniform_real_distribution<double> dist(1.0, 100000.0); return dist(mt); }()));
+                    break;
+                }
+                break;
             }
         }
 
+        // Process Logic
+        
+
+        // Draw to Screen
         window.clear();
         window.display();
     }
